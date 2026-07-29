c8a9537d5a3204aca6a7b247b9ad45bcc4d5a6fd
Hesham Labib <labibindy@gmail.com>
2026-07-29 14:50:35 -0400

    node file with a test3

---
+++ src/app.js
@@ -0,0 +1,13 @@
+function greet(name){
+
+    return `Hello, ${name}! Welcome to our application.`;
+
+}
+
+module.exports = greet;
+
+if (required.main === module) {
+
+    console.log(greet("World"));
+}
+
